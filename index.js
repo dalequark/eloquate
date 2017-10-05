4634b6b960cf9d03b987be9f0ed214670f7d639b
Dale Markowitz <dale@okcupid.com>
2017-10-04 22:16:52 -0400

    added content editable div plus blast

--- index.js
+++ index.js
@@ -14,38 +14,55 @@ $(document)
         this.rows = minRows + rows;
     });
 
-$('textarea').change(function() {
-  console.log("updating stats...");
-  updateStats();
-});
-
 updateStops();
 
 function updateStops() {
     $('#stop_words').text(stop_words.join(', '));
 }
 
-function colorWords(indices, colors) {
-  $('textarea').blast({
+function colorWords(words, color='red') {
+  $('#text_input').blast({
     delimiter: 'word',
-    generateIndexID: true
+    generateIndexID: true,
+    generateValueClass: true,
+    returnGenerated: false
   });
-  $("p").blast({ delimiter: "word" });
+
+  console.log(words);
+
+  for(word in words) {
+    var selector = "#text_input .blast blast-word-" + word;
+    $(selector).css('color', color);
+  }
 }
 
+const min_freq = 2;
+
 function updateStats() {
-  var text = $('textarea').val();
+
+  var text = $('#text_input')[0].innerText;
   var info = analyzeText(text);
   var frequencies = info.frequencies;
   var repeat_distances = info.repeat_distances;
   var top_word = mostFrequent(frequencies);
   $('#word_count').text("Word Count: " + info.word_count);
 
-  var most_frequent = getWordsOverFrequency(frequencies);
+  var most_frequent = getWordsOverFrequency(frequencies, min_freq);
+
   var frequent_str = "";
   most_frequent.forEach(function(word_info) {
     frequent_str += "(\"" + word_info.word + "\", " + word_info.count + ") ";
   });
+
   $("#most_frequent").text(frequent_str);
-  colorWords();
+
+  colorWords(frequencies.keys);
+
 }
+
+// Let the timeout wait for you to finish typing
+var timeout;
+$("#text_input").on("input", function() {
+  clearTimeout(timeout);
+  timeout = setTimeout(updateStats, 1000);
+});
