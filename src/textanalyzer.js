33334ba25430a26b180744e1b93de68556c69585
Dale Markowitz <dale@okcupid.com>
2017-10-12 13:54:07 -0400

    got mocha tests working for repeat words

--- src/textanalyzer.js
+++ src/textanalyzer.js
@@ -1,39 +1,56 @@
 module.exports = function TextAnalysis(input_text) {
 
   let stop_words =
-    ['she', 'he', 'the', 'a', 'and', 'but', 'when', 'I', 'they'];
+  ['she', 'he', 'the', 'a', 'and', 'but', 'when', 'I', 'they'];
   let freq_dict = {};
 
-  // Distance between this and last distance of this word
-  let repeat_dists = [];
+  // Keep track of distance between last word usage
+  let repeat_dists = {};
 
   // Regex delimter matches the one from Blast.js
-  let tokens = input_text.split(/\s*(\S+)\s*/);
-  let word_index = 0;
-  let freq_info;
+  let tokens = input_text.split(/\s*(\S+)\s*/).filter((x) => { return x; });
 
-  tokens.forEach( (word) => {
-    word = word.toLowerCase();
+  for(let i = 0; i < tokens.length; i++) {
+    tokens[i] = tokens[i].toLowerCase();
+  }
+
+
+  tokens.forEach(function(word, word_index) {
 
     if(stop_words.indexOf(word) >= 0 || word.length < 1)  return;
 
-    freq_info = freq_dict[word];
-    if(freq_info) {
-      freq_info['count'] += 1;
-
-      // Positions of this word
-      freq_info['indices'].push(word_index);
-
-      repeat_dists[word_index] = word_index - freq_info[freq_info.length - 1];
-    }
-    else {
+    if(!freq_dict[word]) {
       freq_dict[word] = {'count' : 1, 'indices' : [word_index]};
-      repeat_dists[word_index] = Infinity;
+      return;
     }
-    word_index++;
+
+    freq_info = freq_dict[word];
+
+    freq_info['count'] += 1;
+
+    // Positions of this word
+    let last_index = freq_info['indices'][freq_info['indices'].length - 1];
+    freq_info['indices'].push(word_index);
+
+    // map distance (as key) to index of word
+
+    let word_distance = word_index - last_index;
+    if(repeat_dists[word_distance]) {
+      repeat_dists[word_distance].push(word_index);
+    } else {
+      repeat_dists[word_distance] = [word_index];
+    }
+
   });
 
+
+  /* ------------------------------------------------------------------------*/
+
+  this.tokens = tokens;
+
   this.frequencies = freq_dict;
+
+  // Dictionary of distance_between_last_usage => index of all repeated words
   this.repeat_distances = repeat_dists;
 
   this.getStops = () => { return stop_words; }
@@ -45,9 +62,21 @@ module.exports = function TextAnalysis(input_text) {
     return this.frequencies[word].count || 0;
   }
 
-  this.wordsOverFrequency =  (limit=4) => {
+  this.wordsOverFrequency =  (limit = 4) => {
     return Object.keys(this.frequencies)
-      .filter( word => this.frequencies[word].count >= limit);
+    .filter( word => this.frequencies[word].count >= limit);
+  }
+
+  // Returns indices of words repeated under distance
+  this.repeatsUnderDistance = (min_distance = 10) => {
+    let repeats = [];
+    for(let i = 0; i < min_distance; i++) {
+      if(!this.repeat_distances[i]) continue;
+      this.repeat_distances[i].forEach((index) => {
+        repeats.push([index - i, index]);
+      });
+    }
+    return repeats;
   }
 
 }
--- test.js
+++ test.js
@@ -1,15 +1,19 @@
-var ta = require('./textAnalyzer.js');
+var TextAnalyzer = require('./src/textAnalyzer.js');
 var assert = require('assert');
 
 var text = "This is a test test string, where the most repeated " +
 "words are test, string, and also repeated. But the shows up the " +
 "a whole lot. Am I lying? the the the should be a stopword test test";
 
-var info = ta.analyzeText(text);
+var info = new TextAnalyzer(text);
 
 describe('analyzeText', function() {
 
   it('should not be null', function() { assert(info)} );
+
+  it('should have nonzero length tokens', function() {
+    info.tokens.forEach((token) => { assert(token.length > 0) });
+  });
 
   describe('frequencies', function() {
 
@@ -26,27 +30,60 @@ describe('analyzeText', function() {
 
     it('should not contain stopwords', function() {
       for(word in freqs) {
-        assert(ta.stop_words.indexOf(word) == -1);
+        assert(info.getStops().indexOf(word) == -1);
       }
     });
 
-  });
+    it('should have word indices point to actual words', function() {
+      for(word in freqs) {
+        freqs[word]['indices'].forEach((index) => {
+          assert(info.tokens[index] == word, "For word " + word +
+          " at index " + index + " found word\t" + info.tokens[index]);
+        });
+      }
+    });
 
-  describe('repeat_distances', function() {
-    it('should not be null', function() { assert(info.repeat_distances)});
-  });
+    let repeat_distance = 8;
 
-});
+    describe('repeat distances', function() {
+      var repeats = info.repeatsUnderDistance();
 
-describe('mostFrequent', function() {
-  var res = ta.mostFrequent(info);
+      it('should not be null', function() { assert(repeats); });
 
-  it('it should be "test"', function() {
-      assert(res.word == 'test', res.word);
-  });
+      it('should contain at least one entry', function() {
+        assert(repeats.length > 0);
+      });
 
-  it('should have count 5', function() {
-    assert(res.count == 5, res.count);
+      it('should have entries of [x,x]', function() {
+        assert(repeats[0].length == 2);
+      });
+
+      it('should return positive indices less than length of text', function() {
+        repeats.forEach((pairs) => {
+          assert(pairs[0] >= 0 && pairs[0] < info.tokens.length,
+          "First index: " + pairs[0]);
+
+          assert(pairs[1] >= 1 && pairs[1] < info.tokens.length,
+          "Second index: " + pairs[1]);
+        });
+      });
+
+      it('should return pairs of the same word', function() {
+        repeats.forEach((pairs) => {
+          assert(info.tokens[pairs[0]] == info.tokens[pairs[1]],
+          info.tokens[pairs[0]] + ", " + info.tokens[pairs[0]]);
+        });
+      });
+
+      it('should return words less than or equal to ' + repeat_distance +
+      ' words apart', function() {
+        repeats.forEach((pairs) => {
+          assert(pairs[1] - pairs[0] <= repeat_distance);
+        });
+      });
+
+    });
+
   });
 
 });
